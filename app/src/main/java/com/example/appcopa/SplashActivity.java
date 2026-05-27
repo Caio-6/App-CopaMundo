01f70e95381172205f976774525db9f9e4da4912
Caio-06 <caiobastos0014@gmail.com>
2026-05-27 09:36:56 -0300

    splash screen atualizada

--- app/src/main/java/com/example/appcopa/SplashActivity.java
+++ app/src/main/java/com/example/appcopa/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.appcopa;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +19,21 @@ public class SplashActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
 
+        VideoView video=findViewById(R.id.videoView);
+
+        Uri videoUri= Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash_screen);
+
+        video.setVideoURI(videoUri);
+
+        video.setOnPreparedListener(mp ->{
+            mp.setLooping(false);
+            video.start();
+        });
+
+        video.setOnCompletionListener(mp->{
+            Intent intent = new Intent(SplashActivity.this,SelectLanguage.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
