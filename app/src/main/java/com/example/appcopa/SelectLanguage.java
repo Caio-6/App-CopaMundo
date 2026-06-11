56a90815799ea8a91874be2572057a07e3facd07
Caio <caiobastos0014@gmail.com>
2026-06-10 22:11:15 -0300

    Codigo de escolher lingua

--- app/src/main/java/com/example/appcopa/SelectLanguage.java
+++ app/src/main/java/com/example/appcopa/SelectLanguage.java
@@ -3,6 +3,7 @@ package com.example.appcopa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,40 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import org.intellij.lang.annotations.Language;
+import com.example.appcopa.TBancoDados.ConfiguracaoModel;
 
-import java.util.Locale;
 
 public class SelectLanguage extends AppCompatActivity {
+    private ConfiguracaoModel banco_dados= new ConfiguracaoModel(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_select_language);
-
-        int[] btnIdiomas={R.id.portugues,R.id.english,R.id.espanhol,R.id.nihongo,R.id.koreano,
-                R.id.francais,R.id.italiano,R.id.russo,R.id.portugal};
-        Intent intent=new Intent(SelectLanguage.this,MainActivity.class);
-        View.OnClickListener listenerIdiomas= new View.OnClickListener() {
+        Button btnPortugues=findViewById(R.id.portugues);
+        Intent intent=new Intent(SelectLanguage.this,GrupoSelecao.class);
+        btnPortugues.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int idClick= view.getId();
-
-                if(idClick==R.id.portugues)
-                {
-                    String cdIdioma="pt";
-                }
+                String codIdioma="pt";
+                banco_dados.SalvarLingua(codIdioma);
+                startActivity(intent);
             }
-        }
-        for(int id:btnIdiomas)
-        {
-            Button btn=findViewById(id);
-            btn.setOnClickListener()
-        }
-    }
-    private void mudarIdioma(String codigoIdioma){
-        Locale locale = new Locale(codigoIdioma);
-        Locale.setDefault(Lo);
+        });
     }
 }
