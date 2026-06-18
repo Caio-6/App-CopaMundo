db9c7cc3f62addd753cfbae5cb56b50a81272126
Caio <caiobastos0014@gmail.com>
2026-06-18 18:32:09 -0300

    Configuração do id de lingua

--- app/src/main/java/com/example/appcopa/TBancoDados/util/CodigoLinguaEnum.java
+++ app/src/main/java/com/example/appcopa/TBancoDados/util/CodigoLinguaEnum.java
@@ -1,24 +1,30 @@
 package com.example.appcopa.TBancoDados.util;
 
 public enum CodigoLinguaEnum {
-    PORTUGUES_BR("pt-BR"),
-    PORTUGUES_PT("pt-PT"),
-    INGLES("en"),
-    ESPANHOL("es"),
-    JAPONES("ja"),
-    COREANO("ko"),
-    FRANCES("fr"),
-    RUSSO("ru"),
-    ITALIANO("it");
+    PORTUGUES_BR("pt-BR",1),
+    PORTUGUES_PT("pt-PT",2),
+    INGLES("en",3),
+    ESPANHOL("es",4),
+    JAPONES("ja",5),
+    COREANO("ko",6),
+    FRANCES("fr",7),
+    RUSSO("ru",8),
+    ITALIANO("it",9);
 
     private final String codLingua;
-
-    CodigoLinguaEnum(String codLingua) {
+    private final int id;
+    CodigoLinguaEnum(String codLingua, int id) {
         this.codLingua = codLingua;
+        this.id=id;
     }
 
     public String getCodLingua() {
         return codLingua;
+    }
+
+    public int getId()
+    {
+        return id;
     }
 
     public static CodigoLinguaEnum procLingua(String cod)
