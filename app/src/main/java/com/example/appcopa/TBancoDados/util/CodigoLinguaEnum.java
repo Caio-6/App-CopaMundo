996afd0acc807d82ca87e42a0ddc0ffafa828713
Caio <caiobastos0014@gmail.com>
2026-06-18 18:39:29 -0300

    Remoção do id de lingua

--- app/src/main/java/com/example/appcopa/TBancoDados/util/CodigoLinguaEnum.java
+++ app/src/main/java/com/example/appcopa/TBancoDados/util/CodigoLinguaEnum.java
@@ -1,30 +1,23 @@
 package com.example.appcopa.TBancoDados.util;
 
 public enum CodigoLinguaEnum {
-    PORTUGUES_BR("pt-BR",1),
-    PORTUGUES_PT("pt-PT",2),
-    INGLES("en",3),
-    ESPANHOL("es",4),
-    JAPONES("ja",5),
-    COREANO("ko",6),
-    FRANCES("fr",7),
-    RUSSO("ru",8),
-    ITALIANO("it",9);
+    PORTUGUES_BR("pt-BR"),
+    PORTUGUES_PT("pt-PT"),
+    INGLES("en"),
+    ESPANHOL("es"),
+    JAPONES("ja"),
+    COREANO("ko"),
+    FRANCES("fr"),
+    RUSSO("ru"),
+    ITALIANO("it");
 
     private final String codLingua;
-    private final int id;
-    CodigoLinguaEnum(String codLingua, int id) {
+    CodigoLinguaEnum(String codLingua) {
         this.codLingua = codLingua;
-        this.id=id;
     }
 
     public String getCodLingua() {
         return codLingua;
-    }
-
-    public int getId()
-    {
-        return id;
     }
 
     public static CodigoLinguaEnum procLingua(String cod)
