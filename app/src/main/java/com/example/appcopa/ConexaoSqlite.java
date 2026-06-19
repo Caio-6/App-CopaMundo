9321e84ede736f817493e99b47547fb12a13ea75
Caio <caiobastos0014@gmail.com>
2026-06-18 22:04:31 -0300

    Conexão db

--- app/src/main/java/com/example/appcopa/ConexaoSqlite.java
+++ app/src/main/java/com/example/appcopa/ConexaoSqlite.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.appcopa.TBancoDados.database.DDL.ConfiguracaoDDL;
+import com.example.appcopa.TBancoDados.database.DDL.EstadiosDDL;
 import com.example.appcopa.TBancoDados.database.DDL.GrupoDDL;
 import com.example.appcopa.TBancoDados.database.DDL.GrupoSelecaoDDL;
 import com.example.appcopa.TBancoDados.database.DDL.LinguaDDL;
@@ -13,24 +14,15 @@ import com.example.appcopa.TBancoDados.database.DDL.SelecaoDDL;
 public class ConexaoSqlite extends SQLiteOpenHelper {
 
     // Configurações do Banco de Dados
-    private static final String NOME_BANCO = "DB\\CopaApp.db";
+    private static final String NOME_BANCO = "app_copa.db";
     private static final int VERSAO_BANCO = 1;
 
     // Constantes da Tabela Estadios
-    public static final String TABELA_ESTADIOS = "Estadios";
-    public static final String COL_ID = "Id";
-    public static final String COL_CODIGO_ENUM = "Codigo_estadio_enum";
-    public static final String COL_NOME = "Nome";
-    public static final String COL_LOCAL = "Local";
-    public static final String COL_DESCRICAO = "Descricao";
-    public static final String COL_CAPACIDADE = "Capacidade";
-    public static final String COL_DATA_FUNDACAO = "data_fundacao"; // Alterado hífen para underline (boa prática)
-    public static final String COL_IMAGEM = "imagem";
-    public static final String COL_LINK_MAPS = "link_maps"; // Alterado hífen para underline
 
-    // Construtor da classe
+
     public ConexaoSqlite(Context context) {
         super(context, NOME_BANCO, null, VERSAO_BANCO);
+        // Caminho oficial onde o Android armazena e lê os bancos de dados
     }
 
     //Configuração pra foreign key
@@ -43,21 +35,9 @@ public class ConexaoSqlite extends SQLiteOpenHelper {
     // Método chamado na primeira vez que o banco de dados é criado
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String scriptCriacaoTabela = "CREATE TABLE IF NOT EXISTS " + TABELA_ESTADIOS + " ("
-                + COL_ID + " INTEGER NOT NULL UNIQUE, "
-                + COL_CODIGO_ENUM + " INTEGER NOT NULL UNIQUE, "
-                + COL_NOME + " TEXT NOT NULL, "
-                + COL_LOCAL + " TEXT NOT NULL, "
-                + COL_DESCRICAO + " TEXT NOT NULL, "
-                + COL_CAPACIDADE + " INTEGER NOT NULL, "
-                + COL_DATA_FUNDACAO + " TEXT NOT NULL, "
-                + COL_IMAGEM + " BLOB NOT NULL UNIQUE, "
-                + COL_LINK_MAPS + " TEXT NOT NULL, "
-                + "PRIMARY KEY(" + COL_ID + " AUTOINCREMENT)" // Define o Id como autoincremento
-                + ");";
 
         // Executa o script SQL de criação
-        db.execSQL(scriptCriacaoTabela);
+        db.execSQL(EstadiosDDL.SCRIPT_CRIACAO);
         db.execSQL(LinguaDDL.SCRIPT_CRIACAO);
         db.execSQL(ConfiguracaoDDL.SCRIPT_CRIACAO);
         db.execSQL(SelecaoDDL.SCRIPT_CRIACAO);
@@ -69,7 +49,7 @@ public class ConexaoSqlite extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Por enquanto, deleta a tabela antiga e recria uma nova
-        db.execSQL("DROP TABLE IF EXISTS " + TABELA_ESTADIOS);
+        db.execSQL(EstadiosDDL.SCRIPT_DELELTAR);
         db.execSQL(LinguaDDL.SCRIPT_DELETAR);
         db.execSQL(ConfiguracaoDDL.SCRIPT_DELETAR);
         db.execSQL(SelecaoDDL.SCRIPT_DELETAR);
